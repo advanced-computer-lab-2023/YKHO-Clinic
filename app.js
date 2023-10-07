74c5eef8492624b739e6072531d0ff17c7908c9e
Youssef Safwat <yousseftyoh@gmail.com>
2023-10-08 00:10:11 +0300

    admin

--- app.js
+++ app.js
@@ -44,33 +44,8 @@ app.get("/admin/home/HealthPackages", goToHealthPackages);
 app.post("/admin/home/healthPackages/done", addHealthPackages);
 app.put("/admin/home/healthPackages/done", updateHealthPackages);
 app.delete("/admin/home/healthPackages/done", deleteHealthPackages);
-
-
-
-app.listen(port, () => {
-  console.log(`Listening to requests on http://localhost:${port}`);
-});
-
-mongoose
-  .connect("mongodb://127.0.0.1/clinic")
-  .then(() => console.log("connected to clinicDB"))
-  .catch((err) => console.log(err.message));
-
-const id ="1"
-
-app.use(express.json());
 app.post("/addDoctor",createDoctor);
 app.post("/addAppointment",createAppointment)
 app.get("/Doctor/patients",showMyPatients);
-app.get("view/admin", adminLogin);
-app.get("view/admin/home", adminHome);
-app.get("view/admin/register", adminRegister);
-app.get("view/admin/home/deleteUser", goToDeleteUser);
-app.get("view/admin/home/HealthPackages", goToHealthPackages);
-app.post("view/admin/register/done", createAdmin);
-app.post("view/admin/home/healthPackages/done", addHealthPackages);
-app.put("view/admin/home/healthPackages/done", updateHealthPackages);
-app.delete("view/admin/home/healthPackages/done", deleteHealthPackages);
-app.delete("view/admin/home/deleteUser/done", deleteUser);
 app.get("/Doctor/patients/:id",showMyPatientInfo)
 app.get("/Doctor/upcomingAppointments",showUpcomingAppointments)
