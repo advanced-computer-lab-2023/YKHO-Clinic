de9d7c0ddd477ac530e0223b06219071986511c3
k-i-k-a <100940677+k-i-k-a@users.noreply.github.com>
2023-10-07 20:42:32 +0300

    formatting

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const mongoose = require('mongoose')
 const express = require('express');
-const app= express();
-const id ="1"
-const port = 3000;
 const createDoctor = require('./controller/doctorController');
 const {createAppointment,showMyPatients} = require('./controller/appointmentController');
+
+const app= express();
+const port = 3000;
 app.listen(port, () => {
   console.log(`Listening to requests on http://localhost:${port}`);
 });
-app.use(express.json());
+
 mongoose
   .connect("mongodb://127.0.0.1/clinic")
   .then(() => console.log("connected to clinicDB"))
   .catch((err) => console.log(err.message));
+
+const id ="1"
+
+app.use(express.json());
 app.post("/addDoctor",createDoctor);
 app.post("/addAppointment",createAppointment)
 app.get("/Doctor/patients",showMyPatients)
