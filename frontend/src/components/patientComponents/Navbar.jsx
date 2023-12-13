4a8e2226d85589478fa4cb253b37b1716800a230
Maged <yomamo77@gmail.com>
2023-12-14 01:18:33 +0200

    error removal

--- frontend/src/components/patientComponents/Navbar.jsx
+++ frontend/src/components/patientComponents/Navbar.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { styled, alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
